c9eb6712d3bc2bc98245b2f939d2ae712c9e05cf
sarvan <sarvan@boi.com>
2025-04-24 06:03:10 +0000

    added subtraction and sign handling

--- arbitraryarithmetic/Ainteger.java
+++ arbitraryarithmetic/Ainteger.java
@@ -45,6 +45,26 @@ public class Ainteger {
     public StringBuilder add(Ainteger a){
         StringBuilder thisnum = new StringBuilder(num);
         StringBuilder othernum = new StringBuilder(a.get());
+        if(num.charAt(0)=='-'&&a.get().charAt(0)=='-'){
+            othernum.deleteCharAt(0);
+            thisnum.deleteCharAt(0);
+            Ainteger temp = new Ainteger(thisnum.toString());
+            StringBuilder result  = new StringBuilder(temp.add(new Ainteger(othernum.toString())));
+            result.insert(0, '-');
+            return result;
+        }
+        else if(num.charAt(0)=='-'){
+            thisnum = new StringBuilder(num);
+            thisnum.deleteCharAt(0);
+            StringBuilder result  = new StringBuilder(a.sub(new Ainteger(thisnum.toString())));
+            return result;
+        }
+        else if(a.get().charAt(0)=='-'){
+            othernum = new StringBuilder(a.get());
+            othernum.deleteCharAt(0);
+            StringBuilder result = new StringBuilder(this.sub(new Ainteger(othernum.toString())));
+            return result;
+        }
         String num1=thisnum.toString();
         String num2=othernum.toString();
         while(num1.length()<num2.length()){
@@ -68,6 +88,62 @@ public class Ainteger {
     }
     public String addition(Ainteger a){
         String ans = this.add(a).toString();
+        return remove_zeroes(ans);
+    }
+    public StringBuilder sub(Ainteger a){
+        StringBuilder thisnum = new StringBuilder(num);
+        StringBuilder othernum = new StringBuilder(a.get());
+        if(num.charAt(0)=='-'&&a.get().charAt(0)=='-'){
+            othernum.deleteCharAt(0);
+            thisnum.deleteCharAt(0);
+            a = new Ainteger(othernum.toString());
+            StringBuilder result  = new StringBuilder(a.sub(new Ainteger(thisnum.toString())));
+            return result;
+        }
+        else if(num.charAt(0)=='-'){
+            thisnum.deleteCharAt(0);
+            StringBuilder result  = new StringBuilder(a.add(new Ainteger(thisnum.toString())));
+            result.insert(0, '-');
+            return result;
+        }
+        else if(a.get().charAt(0)=='-'){
+            othernum.deleteCharAt(0);
+            StringBuilder result  = new StringBuilder(this.add(new Ainteger(othernum.toString())));
+            return result;
+        }
+        String num1=thisnum.toString();
+        String num2=othernum.toString();
+        int check = 0;
+        while(num2.length()<num1.length()){
+            num2="0"+num2;
+        }
+        while(num1.length()<num2.length()){
+            num1="0"+num1;
+        }
+        if(num1.compareTo(num2)<0){
+            String temp = num1;
+            num1=num2;
+            num2=temp;
+            check=1;
+        }
+        int size = num2.length();
+        int current_dif=0;
+        int carry=0;
+        StringBuilder result = new StringBuilder(num1);
+        for(int i =size-1;i>=0;i--){
+            current_dif=result.charAt(i)-num2.charAt(i)-carry;
+            if(current_dif<0){
+                current_dif+=10;
+                carry=1;
+            }
+            else carry=0;
+            result.setCharAt(i,(char)(current_dif+48));
+        }
+        if(check==1) result.insert(0,'-');
+        return result;
+    }
+    public String subtract(Ainteger a){
+        String ans = this.sub(a).toString();
         return remove_zeroes(ans);
     }
 }
